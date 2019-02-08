a5adff8a0b1e65d2adcccdd3789d135649f61438
Antonio <Antonio@LAPTOP-21RVCJ6E>
2019-02-08 16:13:18 +0100

    Creación de RangoException

---
+++ src/ejercicio_excepciones_9/Ejercicio_excepciones_9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_excepciones_9;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Ejercicio_excepciones_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/ejercicio_excepciones_9/RangoException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_excepciones_9;
+
+/**
+ *
+ * @author Antonio
+ */
+public class RangoException extends Exception 
+{
+     public  RangoException (String mensaje)
+  {
+      super(mensaje);
+  }
+}
